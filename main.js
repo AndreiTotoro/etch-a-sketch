f4d2f7fe2a9179ea7420a5d78b489f14f02aa86b
AndreiTotoro <AndreiTotoro@gmail.com>
2022-05-28 20:52:28 +0300

    Make one square div
    Add one square div using javascript to decide on the look

---
+++ main.js
@@ -0,0 +1,7 @@
+//Style one square div
+const grid = document.querySelector(`#grid`);
+const square = document.createElement(`div`);
+square.classList.toggle('square');
+
+grid.appendChild(square);
+//Create a 16 x 16  grid of square divs
