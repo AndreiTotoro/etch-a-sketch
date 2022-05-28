fb741302aca10f42a79ed48ddf5de1eb13d6f98c
AndreiTotoro <AndreiTotoro@gmail.com>
2022-05-28 20:58:50 +0300

    Make a 16x16 grid of square divs
    Generate a 16 x 16 grid of square divs to serve as the canvas for the etch-a-sketch board.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 //Style one square div
 const grid = document.querySelector(`#grid`);
-const square = document.createElement(`div`);
-square.classList.toggle('square');
 
-grid.appendChild(square);
 //Create a 16 x 16  grid of square divs
+
+for (let i = 1; i <= 16 * 16; i++) {
+	const square = document.createElement(`div`);
+	square.classList.toggle('square');
+	grid.appendChild(square);
+}
