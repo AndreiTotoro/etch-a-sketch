6267c0a929cf95b7c596f23dded1d94b4388cc29
AndreiTotoro <AndreiTotoro@gmail.com>
2022-05-31 10:47:45 +0300

    Add borders to the div

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//Style one square div
+//Get elements from dom
 const grid = document.querySelector(`#grid`);
 const changeButton = document.querySelector(`#change-grid`)
 
-//Create a 16 x 16  grid of square divs
+
+//Change the size of the current grid
 const makeGrid = (size) => {
 
 const oldgrid = document.querySelectorAll('.square')
@@ -13,13 +14,13 @@ oldgrid.forEach((div) => {
 for (let i = 1; i <= size * size; i++) {
 	const square = document.createElement(`div`);
 	square.classList.toggle('square');
-	square.style.height = `${100.0 / size}%`
-	square.style.width = `${100.0 / size}%`
+	square.style.height = `${800.0 / size - 2}px`
+	square.style.width = `${800.0 / size - 2}px`
 	grid.appendChild(square);
 }
 
 }
-
+//Update the grid with the value the user gives
 const gridChanger = () => {
 	makeGrid(prompt("What size grid do you want?"))
 }
